c38b9006144162184a53ae01748bfa44aac98f0c
jumpeiminami <92559616+jumepiminami@users.noreply.github.com>
2022-01-24 16:18:07 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-function sliceMaxLength(minLength, elem) {  
-    elem.value = elem.value.slice(minLength, 10);  
-}  
-
-
-
+//入力桁数制限
 function inputCheck () {
   var form = document.getElementById("studentNumber");
   var form_value = form.value;
   
   if (form_value.match(/[0-9]{10}/g) != form_value ) {
-  	document.getElementById('alert').innerHTML = '10桁の数字を入力してください。';
+  	document.getElementById('alert').innerHTML = '10桁の学籍番号を<br>入力してください。';
     form.value = '';
   } else {
   	document.getElementById('alert').innerHTML = 'OKです';  
@@ -17,3 +12,13 @@ function inputCheck () {
   
   document.getElementById('output').innerHTML = form_value;
 }
+
+
+
+   function beforeSubmit() {
+    if(window.confirm('この内容で送信しますがよろしいでしょうか？')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
