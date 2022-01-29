5b50ee3c4e9b14596bf748c2164aedf6e30b3a99
jumpeiminami <92559616+jumepiminami@users.noreply.github.com>
2022-01-29 16:40:22 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -25,32 +25,96 @@ $(window).on('load', function () {
 
 
 
+
 //入力桁数制限
 function inputCheck () {
   var form = document.getElementById("studentNumber");
   var form_value = form.value;
   
   if (form_value.match(/[0-9]{10}/g) != form_value ) {
-  	document.getElementById('alert').innerHTML = '10桁の学籍番号を<br>入力してください。';
+  	document.getElementById('alert2').innerHTML = '10桁の学籍番号を<br>入力してください。';
     form.value = '';
   } else {
-  	document.getElementById('alert').innerHTML = 'OKです';  
+  	document.getElementById('alert2').innerHTML = 'OKです';  
   }
   
   document.getElementById('output').innerHTML = form_value;
 }
 
 
+function check(){
+
+	var flag = 0;
+
+
+	// 設定開始（必須にする項目を設定してください）
+
+	if(document.form1.name.value == ""){ // 「お名前」の入力をチェック
+
+		flag = 1;
+
+	}
+	else if(document.form1.studentnumber.value == ""){ // 「学籍番号」の入力をチェック
+
+		flag = 1;
+
+	}
+	else if (document.form1.email.value == "") {  // 「メアド」の入力をチェック
+
+		flag = 1;
+	}
+	else if(document.form1.state.value == ""){ // 「state」の入力をチェック
+
+		flag = 1;
+
+
+	}
+	else if (document.form1.device.value == "") {  // 「device」の入力をチェック
+		flag = 1;
+	}
+	else if (document.form1.otoiawase.value == "") {  // 「otoiawase」の入力をチェック
+		flag = 1;
+	}
+
+	// 設定終了
+
+
+	if(flag){
+
+		window.alert('必須項目に未入力がありました'); // 入力漏れがあれば警告ダイアログを表示
+		return false; // 送信を中止
+
+	}
+	else{
+
+		return beforeSubmit();
+	}
+
+}
+
+function fa(){
+	document.getElementById('alert1').innerHTML = '必須です';
+	form.value = '';
+	document.getElementById('alert3').innerHTML = '必須です';
+	form.value = '';
+	document.getElementById('alert4').innerHTML = '必須です';
+	form.value = '';
+	document.getElementById('alert5').innerHTML = '必須です';
+	form.value = '';
+	document.getElementById('alert6').innerHTML = '必須です';
+    form.value = '';
+	
+}
+
 //送信確認
 function beforeSubmit() {
-    if(window.confirm('この内容で送信しますがよろしいでしょうか?')) {
-        return true;
-    } else {
-      return false;
+    if(window.alert('この内容で送信しますがよろしいでしょうか?')) {
+		return true;
+	} else {
+		return false;
     }
 }
    
 
-  
 
 
